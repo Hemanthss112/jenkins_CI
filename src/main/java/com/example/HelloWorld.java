18f986103c18aeec9d475eccc11c6a6841b7341f
Hemanth S S <135803347+Hemanthss112@users.noreply.github.com>
2026-03-15 22:13:02 -0700

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.println("hello from jenkins ci pipeline");
+        System.out.println("hi from jenkins ci pipeline");
     }
 }
